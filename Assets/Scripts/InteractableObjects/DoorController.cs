f6689b71e2b54fddbf449d211fe3e756f9414c4e
david.cubias <david.cubias@students.salle.url.edu>
2024-03-18 20:26:32 +0100

    Efectes de so de les portes + importacio de tots

--- Assets/Scripts/InteractableObjects/DoorController.cs
+++ Assets/Scripts/InteractableObjects/DoorController.cs
@@ -7,9 +7,21 @@ public class DoorController : InteractableObject
 {
     [SerializeField] private Sprite _closedSprite;
     [SerializeField] private Sprite _openSprite;
+    [SerializeField] private AudioClip _openSound;
+    [SerializeField] private AudioClip _closeSound;
 
     private bool isOpen;
     private bool _hasInteracted;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            _audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     protected override void Interact()
     {
@@ -23,5 +35,14 @@ public class DoorController : InteractableObject
         GetComponent<SpriteRenderer>().sprite = isOpen ? _closedSprite : _openSprite;
         GetComponent<Collider2D>().enabled = isOpen;
         isOpen = !isOpen;
+
+        if (isOpen && _openSound != null)
+        {
+            _audioSource.PlayOneShot(_openSound);
+        }
+        else if (!isOpen && _closeSound != null)
+        {
+            _audioSource.PlayOneShot(_closeSound);
+        }
     }
 }
--- Assets/Scripts/InventoryItems/MonoBehaviour/KeyScript.cs
+++ Assets/Scripts/InventoryItems/MonoBehaviour/KeyScript.cs
@@ -6,9 +6,31 @@ using UnityEngine.InputSystem;
 public class KeyScript : SceneInventoryItem
 {
     [SerializeField] private int _id;
+    //[SerializeField] private AudioClip _disappearSound;
+
+    //private AudioSource _audioSource;
+
+    /*private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            _audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }*/
 
     protected override void PickUp()
     {
         _playerController.AddItem(new Key(_id));
-    } 
+        Destroy(gameObject);
+    }
+
+    /*private void OnDestroy()
+    {
+        // Reproducir el sonido cuando el objeto se destruye
+        if (_disappearSound != null)
+        {
+            _audioSource.PlayOneShot(_disappearSound);
+        }
+    }*/
 }
